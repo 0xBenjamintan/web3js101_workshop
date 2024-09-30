cc29da1f5553286a94aa9381c7f80eb644c878ad
Benjamin | benjamintan.eth <124853084+0xBenjamintan@users.noreply.github.com>
2024-09-30 09:05:00 +0000

    added reverse decode

--- decode.ts
+++ decode.ts
@@ -1,6 +1,19 @@
-import wallet from "./dev-wallet.json";
+// import wallet from "./dev-wallet.json";
 
-const bs58 = require("bs58");
-//add wallet variable in the bracket
-const privkey = new Uint8Array(); // content of id.json here
-console.log(bs58.encode(privkey));
+// const bs58 = require("bs58");
+// //add wallet variable in the bracket
+// const privkey = new Uint8Array(wallet); // content of id.json here
+// console.log(bs58.encode(privkey));
+
+
+import bs58 from 'bs58';
+
+// Example Base58 string (the encoded private key)
+const base58String = "";
+
+// Decode Base58 string back into Uint8Array
+const decodedPrivKey = bs58.decode(base58String);
+
+console.log("Decoded Private Key (Uint8Array):", decodedPrivKey);
+
+// Now, decodedPrivKey is the original Uint8Array, which you can use as a private key
